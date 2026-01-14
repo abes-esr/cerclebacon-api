11b1e6c40e866fe199c3bb0bcdb0bea3332fe92b
SamuelQuetin <quetin@abes.fr>
2026-01-14 09:13:02 +0100

    Add basic test class for context loading in Spring Boot application

---
+++ src/test/java/fr/abes/cerclebaconapi/CercleBaconApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.abes.cerclebaconapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CercleBaconApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
